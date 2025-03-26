bfd83b0ae0233aad99d810db585a1f9d3e6626f2
Alejandra Quitian <alejandraquitian23@gmail.com>
2025-03-26 11:59:47 -0400

    update

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 
-const users = ["Alejandra Quitian","Ana Maria Marin","Andrea Rojas","Camilo Salcedo","Cristian Cardona","Cristian Valderrama","Daniel Pinto","Johanna Arboleda","Juliana Perez","Leidy Garcia","Lidsay Perez","Liliana Munoz","Marggie Monroy","Nicolas Velasquez","Santiago Rico","Stefanny Malaver","Valeria Ossa"]
+class users = {
+    builder(name,authorized){
+        this.name = name
+        this.authorized = authorized
+    }
+}
 
+const user1 = new users("Alejandra",true)
+const user2 = new users("Ana",false)
+const user3 = new users("Johanna",true)
+const user4 = new users("Juliana",false)
+const user5 = new users("Liliana",true)
 
 function LogIn(){
     let attempts = 1
