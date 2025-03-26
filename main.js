1c5c74afaf029d7b642c202fd3af9622292b5621
Alejandra Quitian <alejandraquitian23@gmail.com>
2025-03-26 18:27:30 -0400

    Fix

--- main.js
+++ main.js
@@ -1,42 +1,48 @@
-
-class users = {
-    builder(name,authorized){
-        this.name = name
-        this.authorized = authorized
+class Users {
+    constructor(name, authorized) {
+        this.name = name;
+        this.authorized = authorized;
     }
 }
 
-const user1 = new users("Alejandra",true)
-const user2 = new users("Ana",false)
-const user3 = new users("Johanna",true)
-const user4 = new users("Juliana",false)
-const user5 = new users("Liliana",true)
+const usersList = [
+    new Users("Alejandra", true),
+    new Users("Ana", false),
+    new Users("Johanna", true),
+    new Users("Juliana", false),
+    new Users("Liliana", true)
+];
 
-function LogIn(){
-    let attempts = 1
-    let identify = true
+function LogIn() {
+    let attempts = 1;
+    let identify = true;
 
-    
-    do{
-        let user = prompt("Type your user").toUpperCase()
-        let search = users.filter( (x)=> x.toUpperCase().includes(user) )
-        if(user === null){
-            break
+    do {
+        let userInput = prompt("Type your user");
+
+        if (userInput === null) {
+            break;
         }
-        if(search.length >0 && attempts <=3){
-            alert("Welcome " + search)
-            identify =false
-        }else{
-            alert("Jumm... That is not one of our users")
-            attempts++
-            if(attempts > 3){
-                alert("You have exceeded the number of attempts, try again later")
-                break
+
+        userInput = userInput.toLowerCase();
+
+        let foundUser = usersList.find(user => user.name.toLowerCase() === userInput);
+
+        if (foundUser && attempts <= 3) {
+            alert("Welcome " + foundUser.name);
+            identify = false;
+        } else {
+            alert("Jumm... That is not one of our users");
+            attempts++;
+
+            if (attempts > 3) {
+                alert("You have exceeded the number of attempts, try again later");
+                break;
             }
         }
-    }while(identify)
+    } while (identify);
 }
 
 
-LogIn()
+LogIn();
 
